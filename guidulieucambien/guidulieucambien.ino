5e1e1eaa5042778ea3e74a2794f204bc77677857
Nguyễn Quang Tùng <quangtung280609@gmail.com>
2025-12-20 10:18:45 +0700

    Add files via upload

---
+++ guidulieucambien/guidulieucambien.ino
@@ -0,0 +1,65 @@
+#include <WiFi.h>
+#include "secrets.h"
+#include "ThingSpeak.h" // always include thingspeak header file after other header files and custom macros
+
+char ssid[] = SECRET_SSID;   // your network SSID (name) 
+char pass[] = SECRET_PASS;   // your network password
+int keyIndex = 0;            // your network key Index number (needed only for WEP)
+WiFiClient  client;
+
+unsigned long myChannelNumber = SECRET_CH_ID;
+const char * myWriteAPIKey = SECRET_WRITE_APIKEY;
+#include "DHT.h"
+
+#define DHTPIN 4     
+#define DHTTYPE DHT11  
+
+DHT dht(DHTPIN, DHTTYPE);
+void setup() {
+  Serial.begin(115200);  //Initialize serial
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo native USB port only
+  }
+  
+  dht.begin();
+  WiFi.mode(WIFI_STA);   
+  ThingSpeak.begin(client);  // Initialize ThingSpeak
+}
+
+void loop() {
+
+  // Connect or reconnect to WiFi
+  if(WiFi.status() != WL_CONNECTED){
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(SECRET_SSID);
+    while(WiFi.status() != WL_CONNECTED){
+      WiFi.begin(ssid, pass);  // Connect to WPA/WPA2 network. Change this line if using open or WEP network
+      Serial.print(".");
+      delay(5000);     
+    } 
+    Serial.println("\nConnected.");
+  }
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  // set the fields with the values
+  ThingSpeak.setField(1, t);
+  ThingSpeak.setField(2, h);
+
+  // write to the ThingSpeak channel
+  int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+  if(x == 200){
+    Serial.println("Channel update successful.");
+  }
+  else{
+    Serial.println("Problem updating channel. HTTP error code " + String(x));
+  }
+   Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.println(F("°C "));
+  
+  delay(20000); // Wait 20 seconds to update the channel again
+}
---
+++ guidulieucambien/secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "BKSTAR_T2_01"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "stemstar"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 3202138			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "V2Z2DBEAE8GKMWC9"
